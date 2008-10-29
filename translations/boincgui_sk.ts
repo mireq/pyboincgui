2d53319e76c410f832e397e724cb0ced755c143d
Miroslav Bendík <mirec@kofola.(none)>
2008-10-29 09:27:02 +0100

    Pridane slovenske preklady

---
+++ translations/boincgui_sk.ts
@@ -0,0 +1,148 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="sk">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="BoincGui/mainwindow.py" line="15"/>
+        <source>Boinc gui</source>
+        <translation>Boinc gui</translation>
+    </message>
+    <message>
+        <location filename="BoincGui/mainwindow.py" line="19"/>
+        <source>Ready</source>
+        <translation>Pripravený</translation>
+    </message>
+    <message>
+        <location filename="BoincGui/mainwindow.py" line="43"/>
+        <source>&amp;Add Client</source>
+        <translation>&amp;Pridať klienta</translation>
+    </message>
+    <message>
+        <location filename="BoincGui/mainwindow.py" line="44"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Koniec</translation>
+    </message>
+    <message>
+        <location filename="BoincGui/mainwindow.py" line="47"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="BoincGui/mainwindow.py" line="54"/>
+        <source>&amp;File</source>
+        <translation>&amp;Súbor</translation>
+    </message>
+    <message>
+        <location filename="BoincGui/mainwindow.py" line="30"/>
+        <source>Connection error: %1</source>
+        <translation type="obsolete">Chyba spojenia: %1</translation>
+    </message>
+    <message>
+        <location filename="BoincGui/mainwindow.py" line="32"/>
+        <source>Communication error: %1</source>
+        <translation type="obsolete">Chyba komunikácie: %1</translation>
+    </message>
+    <message>
+        <location filename="BoincGui/mainwindow.py" line="34"/>
+        <source>Unknown error: %1</source>
+        <translation type="obsolete">Neznáma chyba: %1</translation>
+    </message>
+</context>
+<context>
+    <name>addClientWizard</name>
+    <message>
+        <location filename="BoincGui/addclientwizard.py" line="127"/>
+        <source>Add Client</source>
+        <translation>Pridať klienta</translation>
+    </message>
+</context>
+<context>
+    <name>clientTreeWidget</name>
+    <message>
+        <location filename="BoincGui/clienttree.py" line="37"/>
+        <source>(connected)</source>
+        <translation>(pripojený)</translation>
+    </message>
+    <message>
+        <location filename="BoincGui/clienttree.py" line="39"/>
+        <source>(disconnected)</source>
+        <translation>(nepripojený)</translation>
+    </message>
+</context>
+<context>
+    <name>connectionPage</name>
+    <message>
+        <location filename="BoincGui/addclientwizard.py" line="43"/>
+        <source>&amp;Port</source>
+        <translation>&amp;Port</translation>
+    </message>
+    <message>
+        <location filename="BoincGui/addclientwizard.py" line="50"/>
+        <source>P&amp;assword</source>
+        <translation>&amp;Heslo</translation>
+    </message>
+    <message>
+        <location filename="BoincGui/addclientwizard.py" line="26"/>
+        <source>Connect to BOINC</source>
+        <translation>Pripojenie na klienta BOINC</translation>
+    </message>
+    <message>
+        <location filename="BoincGui/addclientwizard.py" line="27"/>
+        <source>Settings for connection to BOINC client</source>
+        <translation>Nastavenie pripojenia na klienta BOINC</translation>
+    </message>
+    <message>
+        <location filename="BoincGui/addclientwizard.py" line="29"/>
+        <source>Path to client</source>
+        <translation>Cesta ku klientovi</translation>
+    </message>
+    <message>
+        <location filename="BoincGui/addclientwizard.py" line="33"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="BoincGui/addclientwizard.py" line="39"/>
+        <source>&amp;Address of computer where BOINC is  running</source>
+        <translation>&amp;Adresa počítača na ktorom beží BOINC</translation>
+    </message>
+    <message>
+        <location filename="BoincGui/addclientwizard.py" line="94"/>
+        <source>Choose dir where BOINC is running</source>
+        <translation>Vyberte adresár v ktorom beží BOINC</translation>
+    </message>
+    <message>
+        <location filename="BoincGui/addclientwizard.py" line="111"/>
+        <source>File %1 does not exist</source>
+        <translation>Súbor %1 neexistuje</translation>
+    </message>
+    <message>
+        <location filename="BoincGui/addclientwizard.py" line="114"/>
+        <source>Authorisation file is %1</source>
+        <translation>Autorizačný súbor je %1</translation>
+    </message>
+</context>
+<context>
+    <name>introPage</name>
+    <message>
+        <location filename="BoincGui/addclientwizard.py" line="7"/>
+        <source>Add Client</source>
+        <translation>Pridať klienta</translation>
+    </message>
+    <message>
+        <location filename="BoincGui/addclientwizard.py" line="8"/>
+        <source>Please select a type of your client.</source>
+        <translation>Vyberte prosím typ klienta.</translation>
+    </message>
+    <message>
+        <location filename="BoincGui/addclientwizard.py" line="11"/>
+        <source>Local client</source>
+        <translation>Lokálny klient</translation>
+    </message>
+    <message>
+        <location filename="BoincGui/addclientwizard.py" line="12"/>
+        <source>Remote client</source>
+        <translation>Vzdialený klient</translation>
+    </message>
+</context>
+</TS>
